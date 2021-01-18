e5787ce0c37c4abc6cd13bba27362e1201ef03a8
alialpay <alialpay96@hotmail.com>
2021-01-18 20:32:56 +0300

    update

---
+++ CampIntro/Matematik/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.topla(5, 6);
+            dortIslem.topla(6, 9);
+        }
+    }
+}
