ced6b318a5ded3f826081301de72f71d9958cf0b
Ali Alpay <alialpay96@hotmail.com>
2021-01-31 18:01:59 +0300

    upload

---
+++ Homework_Day7/LinqProject/LinqProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
