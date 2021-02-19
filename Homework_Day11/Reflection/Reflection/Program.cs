ce541f07a47ab3bfdaf2656fda5d1c5c83b029a3
Ali Alpay <alialpay96@hotmail.com>
2021-02-19 23:04:54 +0300

    reflection

---
+++ Homework_Day11/Reflection/Reflection/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DortIslem dortIslem = new DortIslem(2,3);
+            
+            //dortIslem.Topla2();
+            //Console.WriteLine(dortIslem.Topla(3, 3));
+            //Console.WriteLine(dortIslem.Topla2());
+
+            var type = typeof(DortIslem);
+
+            DortIslem dortIslem1 = (DortIslem)Activator.CreateInstance(type,6,9);
+            Console.WriteLine(dortIslem1.Topla(4, 5));
+            Console.WriteLine(dortIslem1.Topla2());
+
+        }
+    }
+    public class DortIslem
+    {
+        int _sayi1;
+        int _sayi2;
+        public DortIslem(int sayi1, int sayi2)
+        {
+            _sayi1 = sayi1;
+            _sayi2 = sayi2;
+        }
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+        public int Topla2()
+        {
+            return _sayi1 + _sayi2;
+        }
+        public int Carp2()
+        {
+            return _sayi1 * _sayi2;
+        }
+    }
+}
